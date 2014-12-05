b9bd720d20a0f5c9949a51dc4063b516d3709410
ndn247 <nampdnguyen@gmail.com>
2014-12-04 19:42:45 -0600

    just says PRICELESS

--- NicolasCageApp/src/com/example/nicolascageapp/CageCluesScore.java
+++ NicolasCageApp/src/com/example/nicolascageapp/CageCluesScore.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
 
 public class CageCluesScore extends Activity {
 
@@ -11,6 +13,12 @@ public class CageCluesScore extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cage_clues_score);
+		
+		ViewFlipper mFlipper;
+		mFlipper = ((ViewFlipper)findViewById(R.id.flipper));
+		mFlipper.startFlipping();
+		mFlipper.setInAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in));
+		mFlipper.setOutAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out));
 	}
 
 	@Override

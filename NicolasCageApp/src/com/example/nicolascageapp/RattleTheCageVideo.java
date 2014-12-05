ff63b408febb1d6bb189f568e042656c03f37bbb
ndn247 <nampdnguyen@gmail.com>
2014-12-04 19:08:29 -0600

    save state of score in rattle the cage

--- NicolasCageApp/src/com/example/nicolascageapp/RattleTheCageVideo.java
+++ NicolasCageApp/src/com/example/nicolascageapp/RattleTheCageVideo.java
@@ -2,7 +2,10 @@ package com.example.nicolascageapp;
 import java.io.File;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,6 +19,8 @@ public class RattleTheCageVideo extends Activity
 {
 	//String SrcPath = "http://youtu.be/diQhM7HLNG8";
 	VideoView myVideoView;
+	
+	static final int RATTLE_SCORE = 50;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -25,24 +30,32 @@ public class RattleTheCageVideo extends Activity
 
 		setContentView(R.layout.rattle_end_video);
 
+		final SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
 		// all videos
 		int[] videos = {R.raw.not_the_bees, R.raw.im_a_vampire, R.raw.love_me, R.raw.america_going_down_the_drain};
 
 		// grab random video
 		int video = videos[(int)(Math.random()*videos.length)];
-
+		
+		
 		myVideoView = (VideoView)findViewById(R.id.RattleTheCageVideo);
 		myVideoView.setMediaController(new MediaController(this));
 		myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + video));
 		myVideoView.start();
 		myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
+	
 			@Override
 			public void onCompletion(MediaPlayer mp) {
 
 				// TODO Auto-generated method stub
 				// need code to switch activities
-				Intent intent = new Intent(RattleTheCageVideo.this, MainActivity.class);
+				Editor editor = prefs.edit();
+				editor.putInt("rattle_most_recent_score", RATTLE_SCORE);
+				editor.commit();
+				
+				Intent intent = new Intent(RattleTheCageVideo.this, RattleTheCageScore.class);
+				intent.putExtra("score", RATTLE_SCORE);
 				startActivity(intent);
 
 			}

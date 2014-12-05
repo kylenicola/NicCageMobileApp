adc221ff2a3478a25a66a659f62b9c3aa43803cb
Kyle Nicola <kyle.a.nicola@gmail.com>
2014-12-04 20:04:51 -0600

    removing actionbars

--- NicolasCageApp/src/com/example/nicolascageapp/ScoreMenu.java
+++ NicolasCageApp/src/com/example/nicolascageapp/ScoreMenu.java
@@ -16,24 +16,7 @@ public class ScoreMenu extends Activity {
 		setContentView(R.layout.activity_score_menu);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.score_menu, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 	
 	public void onClickRattleTheCageScore(View view) {
 		Intent intent = new Intent(this, RattleTheCageScore.class);
